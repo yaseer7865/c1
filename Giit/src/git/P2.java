c20e06cdd9418e20ff68e21874c55016664bcd9c
ADMIN <ADMIN@DESKTOP-VN07PQ8>
2025-02-25 20:22:19 +0530

    2file

---
+++ Giit/src/git/P2.java
@@ -0,0 +1,12 @@
+package git;
+
+public class P2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("another file wala merge wala");
+
+	}
+
+}
